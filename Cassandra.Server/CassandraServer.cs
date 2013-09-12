2d3937ccd541e97ae85f301edcb402696feab7be
Donatas Mažionis <donatas.mazionis@gmail.com>
2013-09-12 21:37:46 +0300

    Cassandra server mock

---
+++ Cassandra.Server/CassandraServer.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using Apache.Cassandra;
+using Thrift.Protocol;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace Cassandra.Server
+{
+    public class CassandraServer : Apache.Cassandra.Cassandra.Iface
+    {
+        private static long _id;
+        private readonly TThreadPoolServer _server;
+
+        public CassandraServer(int port)
+        {
+            _server = new TThreadPoolServer(
+                new Apache.Cassandra.Cassandra.Processor(this),
+                new TServerSocket(port),
+                new TFramedTransport.Factory(),
+                new TFramedTransport.Factory(),
+                new TBinaryProtocol.Factory(),
+                new TBinaryProtocol.Factory(),
+                256, 8192,
+                Log);
+        }
+
+        public void Start()
+        {
+            _server.Serve();
+        }
+
+        private static void Log(string message)
+        {
+            LogFormat(message);
+        }
+
+        [Conditional("DEBUG")]
+        private static void LogFormat(string message, params object[] args)
+        {
+            Console.WriteLine("[Server Log] {0}", String.Format(message, args));
+        }
+
+        private static ColumnOrSuperColumn CreateSuperColumn(int columnCount)
+        {
+            var id = Interlocked.Increment(ref _id);
+            var columns = new List<Column>();
+
+            for (var i = 0; i < columnCount; i++)
+            {
+                var column = new Column
+                                 {
+                                     Name = GetBytes("Column#{0}#{1}", id, i + 1),
+                                     Value = GetBytes("Value#{0}#{1}", id, i + 1),
+                                     Timestamp = Stopwatch.GetTimestamp(),
+                                     Ttl = (int)(id % 1000)
+                                 };
+
+                columns.Add(column);
+            }
+
+            return new ColumnOrSuperColumn
+                       {
+                           Super_column = new SuperColumn
+                                              {
+                                                  Name = GetBytes("SuperColumn#{0}", id),
+                                                  Columns = columns
+                                              }
+                       };
+        }
+
+        private static byte[] GetBytes(string format, params object[] args)
+        {
+            return Encoding.UTF8.GetBytes(String.Format(format, args));
+        }
+
+        public void set_keyspace(string keyspace)
+        {
+            LogFormat("Keyspace set to {0}.", keyspace);
+        }
+
+        public ColumnOrSuperColumn get(
+            byte[] key,
+            ColumnPath column_path,
+            ConsistencyLevel consistency_level)
+        {
+            var count = BitConverter.ToInt32(key, 0);
+            LogFormat("Key {0}, ColumnPath {1}, ConsistencyLevel {2}, Count {3}.", key, column_path, consistency_level, count);
+            return CreateSuperColumn(count);
+        }
+
+        public void login(AuthenticationRequest auth_request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ColumnOrSuperColumn> get_slice(
+            byte[] key,
+            ColumnParent column_parent,
+            SlicePredicate predicate,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int get_count(
+            byte[] key,
+            ColumnParent column_parent,
+            SlicePredicate predicate,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<byte[], List<ColumnOrSuperColumn>> multiget_slice(
+            List<byte[]> keys,
+            ColumnParent column_parent,
+            SlicePredicate predicate,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<byte[], int> multiget_count(
+            List<byte[]> keys,
+            ColumnParent column_parent,
+            SlicePredicate predicate,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<KeySlice> get_range_slices(
+            ColumnParent column_parent,
+            SlicePredicate predicate,
+            KeyRange range,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<KeySlice> get_indexed_slices(
+            ColumnParent column_parent,
+            IndexClause index_clause,
+            SlicePredicate column_predicate,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void insert(
+            byte[] key,
+            ColumnParent column_parent,
+            Column column,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void add(
+            byte[] key,
+            ColumnParent column_parent,
+            CounterColumn column,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove(
+            byte[] key,
+            ColumnPath column_path,
+            long timestamp,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove_counter(
+            byte[] key,
+            ColumnPath path,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void batch_mutate(
+            Dictionary<byte[], Dictionary<string, List<Mutation>>> mutation_map,
+            ConsistencyLevel consistency_level)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void truncate(string cfname)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, List<string>> describe_schema_versions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<KsDef> describe_keyspaces()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string describe_cluster_name()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string describe_version()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TokenRange> describe_ring(string keyspace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string describe_partitioner()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string describe_snitch()
+        {
+            throw new NotImplementedException();
+        }
+
+        public KsDef describe_keyspace(string keyspace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> describe_splits(
+            string cfName,
+            string start_token,
+            string end_token,
+            int keys_per_split)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string system_add_column_family(CfDef cf_def)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string system_drop_column_family(string column_family)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string system_add_keyspace(KsDef ks_def)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string system_drop_keyspace(string keyspace)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string system_update_keyspace(KsDef ks_def)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string system_update_column_family(CfDef cf_def)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CqlResult execute_cql_query(byte[] query, Compression compression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cassandra.Server/Run.cs
@@ -0,0 +1,10 @@
+﻿namespace Cassandra.Server
+{
+    internal static class Run
+    {
+        private static void Main()
+        {
+            new CassandraServer(1337).Start();
+        }
+    }
+}
